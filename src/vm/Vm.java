83a7efe4c4b90c989eead7e02b0e1aaf045abfc7
Litian Zhang <litian_zhang17@163.com>
2020-11-05 21:33:54 -1000

    Create new page table once a new is read and store them into a page table list for later access

--- src/vm/Vm.java
+++ src/vm/Vm.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+
 public class Vm {
 
 	private static ArrayList<String> readFile(String file) throws IOException {	
@@ -16,7 +17,8 @@ public class Vm {
 		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 		String string = "";
 		while((string = bufferedReader.readLine()) != null) {
-			System.out.println(string);
+//			System.out.println(string);
+			data.add(string);
 		}
 		bufferedReader.close();
 		
@@ -33,19 +35,42 @@ public class Vm {
 		
 		Memory memory = new Memory();
 		
-//		for(PageTableEntry p:memory.pageTableEntries) {
+//		for(PageTableEntry p : memory.pageTableEntries) {
 //			System.out.println(p);
 //		}
 //		
-		
 		ArrayList<String> data = new ArrayList<>();
-		
 		data = readFile("C:/fall2020classes/cs415/VirtualMemory/bin/vm/VMInput1.txt");
-		System.out.println(data);
 		
-
+//		System.out.println(data);
 		
+		ArrayList<ArrayList<PageTableEntry>> pageTables = new ArrayList<>();
+		pageTables.add(null);
 		
+		for(String s : data) {
+			String[] line = s.split(" ");
+			if(line[0].equals("new")) {
+				System.out.println("***************************\n new page table create at index before: " + line[1]);
+				ArrayList<PageTableEntry> pageTable = new ArrayList<>();
+				for(int i=0; i<64;i++) {
+					pageTable.add(new PageTableEntry(false, false));
+				}
+				pageTables.add(pageTable);
+				System.out.println("create a page table with size: " + Integer.toString(pageTable.size()));
+				
+			} else if (line[0].equals("switch")) {
+				System.out.println("***************************\n switch page table to index before: " + line[1]);
+			} else if (line[0].equals("access")) {
+				System.out.println("access attempt: " + line[1]);
+			} else {
+				System.out.println("Unknown token");
+			}
+			
+		}
+		
+		System.out.println("Total page tables: " + Integer.toString(pageTables.size()));
+		
+		System.out.println(pageTables.get(1).get(0));
 		
 	}
 
