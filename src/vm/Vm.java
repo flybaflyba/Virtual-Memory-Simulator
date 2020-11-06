2ac0de71c4eca395946265df278427fd0d04d5a6
Litian Zhang <litian_zhang17@163.com>
2020-11-05 21:01:30 -1000

    Read input file

--- src/vm/Vm.java
+++ src/vm/Vm.java
@@ -1,8 +1,29 @@
 package vm;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Vm {
 
-	public static void main(String[] args) {
+	private static ArrayList<String> readFile(String file) throws IOException {	
+		ArrayList<String> data = new ArrayList<>();
+		
+		FileInputStream fileInputStream = new FileInputStream(file);
+		InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		String string = "";
+		while((string = bufferedReader.readLine()) != null) {
+			System.out.println(string);
+		}
+		bufferedReader.close();
+		
+		return data;
+	}
+	
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Aloha Virtual Memory");
 		
@@ -12,10 +33,19 @@ public class Vm {
 		
 		Memory memory = new Memory();
 		
+//		for(PageTableEntry p:memory.pageTableEntries) {
+//			System.out.println(p);
+//		}
+//		
 		
-		for(PageTableEntry p:memory.pageTableEntries) {
-			System.out.println(p);
-		}
+		ArrayList<String> data = new ArrayList<>();
+		
+		data = readFile("C:/fall2020classes/cs415/VirtualMemory/bin/vm/VMInput1.txt");
+		System.out.println(data);
+		
+
+		
+		
 		
 	}
 
